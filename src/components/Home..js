357a8decd765ddf9bd1617c4732a8dcdfd5f5c34
sachu sachin <sachusachin983@gmail.com>
2021-07-24 20:34:40 +0530

    commit

--- src/components/Home..js
+++ src/components/Home..js
@@ -2,13 +2,14 @@ import React from 'react';
 // import {  useState,useEffect } from "react";
 import './home.css';
 import Navbar from "./Navbar";
+import Newsfeed from "./Newsfeed";
 
 function Home({removeAuth,userDetails,token,logout}) {
 
     return (
         <div className="home">
             <Navbar removeAuth={removeAuth} userDetails={userDetails} token={token} logout={logout}/>
-
+            <Newsfeed />
         </div>
     );
 }
