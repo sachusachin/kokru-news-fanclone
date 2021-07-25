b27ff1be4f7faeaf27375a4e78fa7c41f163a94a
sachu sachin <sachusachin983@gmail.com>
2021-07-25 23:34:46 +0530

    commit

--- src/components/Home..js
+++ src/components/Home..js
@@ -2,26 +2,18 @@ import React from 'react';
 // import {  useState,useEffect } from "react";
 import './home.css';
 import Navbar from "./Navbar";
-import {BrowserRouter as Router, Link} from "react-router-dom";
-import { Route,Switch } from "react-router";
 import News from "./News";
-import Newsfeed from "./Newsfeed";
-// import Newsfeed from "./Newsfeed";
-// import News from "./News";
 
 function Home({removeAuth,userDetails,token,logout}) {
 
 
     return (
-        <Router>
             <div className="home">
                 <Navbar removeAuth={removeAuth} userDetails={userDetails} token={token} logout={logout}/>
                 <div className='home__body'>
-                    <Link to="/api">About</Link>
+                   <News />
                 </div>
-                <Route exact path="/api" component={Newsfeed} />
             </div>
-        </Router>
     );
 }
 
