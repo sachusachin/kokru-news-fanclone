4f909718d43e59d1ea7f40dd671990aa8641a3fe
sachu sachin <sachusachin983@gmail.com>
2021-07-30 22:53:31 +0530

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,8 @@
 import React from 'react';
-// import dp from './img/dp1.ico'
+import user from './img/user.png'
+import { Offline, Online } from "react-detect-offline";
 import './navbar.css';
-import {Route} from "react-router";
+// import {Route} from "react-router";
 import {BrowserRouter as Router} from 'react-router-dom';
 import News from "./News";
 // import Logout from "./Logout";
@@ -13,9 +14,13 @@ function Navbar({userDetails,token,logout}) {
             <div className="navbar">
                 <div className='navbar__center'>
                     <div className='navbar__logo'>
-                        <img alt='profile'
-                             src={
-                                 userDetails?.photoURL+"?fields=height,width&access_token="+(token || ltoken)}/>
+                        <Online>
+                            <img alt='profile'
+                                 src={userDetails?.photoURL+"?fields=height,width&access_token="+(token || ltoken)}/>
+                        </Online>
+                        <Offline>
+                            <img src={user} alt='user'/>
+                        </Offline>
                     </div>
                     <div className='navbar__head'>
                         <p>{userDetails?.displayName}</p>
@@ -23,7 +28,6 @@ function Navbar({userDetails,token,logout}) {
                     <div className='navbar__logout'>
                         <button onClick={logout}>logout</button>
                     </div>
-
                 </div>
             </div>
         </Router>
--- src/components/News.js
+++ src/components/News.js
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react';
 // import axios from "axios";
 import './news.css';
+import { Offline, Online } from "react-detect-offline";
+import nonetwork from './img/nonetwork.png';
 
 function News() {
     const [news,setNews] =useState([]);
@@ -26,32 +28,41 @@ function News() {
      console.log(news)
     return(
         <div className='news'>
-            <div className='news__body'>
-                {news.map((data)=>{
-                    if(data.poster_path != null || undefined){
-                        return <div className='content'>
-                            <div className='news__left__top'>
-                                <div className='news__image'>
-                                    <img src={"https://image.tmdb.org/t/p/original/"+data.poster_path} alt={data.title} key={data.title}/>
+            <Online>
+                <div className='news__body'>
+                    {news.map((data)=>{
+                        if(data.poster_path != null || undefined){
+                            return <div className='content'>
+                                <div className='news__left__top'>
+                                    <div className='news__image'>
+                                        <img src={"https://image.tmdb.org/t/p/original/"+data.poster_path} alt={data.title} key={data.title}/>
+                                    </div>
+                                    <div key={data.title} className='link'><a href={data.url+"&width=500"} target='_blank'>{data.original_title}</a></div>
+                                    <div key={data.title} className='bookmark'><button><i className="fal fa-bookmark"> </i></button></div>
                                 </div>
-                                <div key={data.title} className='link'><a href={data.url+"&width=500"} target='_blank'>{data.original_title}</a></div>
-                                <div key={data.title} className='bookmark'><button><i className="fal fa-bookmark"> </i></button></div>
-                            </div>
-                            <div className='news__right__bottom' key={data.title}>
-                                <div className='news__title' key={data.title}>
-                                    <h3 key={data.title}>{data.overview}</h3>
+                                <div className='news__right__bottom' key={data.title}>
+                                    <div className='news__title' key={data.title}>
+                                        <h3 key={data.title}>{data.overview}</h3>
+                                    </div>
+                                    <div key={data.title} className='news__time'>
+                                        <p>release_date : {data.release_date}</p>
+                                    </div>
                                 </div>
-                                {/*<div className='news__discription' key={data.title}>*/}
-                                {/*    <p key={data.title}>{data.description}</p>*/}
-                                {/*</div>*/}
-                                <div key={data.title} className='news__time'>
-                                    <p>release_date : {data.release_date}</p>
-                                </div>
-                            </div>
-                        </div>;
-                    }
-                })}
-            </div>
+                            </div>;
+                        }
+                    })}
+                </div>
+            </Online>
+            <Offline>
+                <div className='news__offline'>
+                    <div className='news__offline__image'>
+                        <img src={nonetwork} alt='no-network'/>
+                    </div>
+                    <div className='news__offline__text'>
+                         Oops! Check your Internet Connection
+                    </div>
+                </div>
+            </Offline>
         </div>
     )
 }
